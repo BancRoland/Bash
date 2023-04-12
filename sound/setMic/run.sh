25893848350ba0d309efb3bc973557087903300c
BancRoland <bancroland@gmail.com>
2023-04-13 00:24:04 +0200

    .

---
+++ sound/setMic/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+amixer -D pulse sset Capture 80%     #ez múködik a webkameránál
+#amixer sset 'Internal Mic Boost',0 0%
+#amixer sset 'Mic Jack',0 0%
+# amixer sset 'Headphone Mic Boost',0 1 #ez működik mikrofonnál
