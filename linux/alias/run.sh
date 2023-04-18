1c7786c7182fc041f357c2bd1d014cc5f1369055
kuvik_PC <bancroland@gmail.com>
2023-04-18 17:14:24 +0200

    .

--- linux/alias/run.sh
+++ linux/alias/run.sh
@@ -2,3 +2,4 @@
 
 # echo alias cD="cd ~/Desktop" >> ~/.bashrc
 # echo alias GIT="cD; bash .gitupdate.sh" >> ~/.bashrc
+# echo alias C_COMPILE; for i in *.c; do gcc $i -lm -o ${i%.*}.out ; done >> ~/.bashrc
