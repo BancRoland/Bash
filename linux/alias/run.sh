0956302cde7678d682b02f99661d3094f996241e
roland_dell <bancroland@gmail.com>
2023-07-03 21:31:12 +0200

    .

--- linux/alias/run.sh
+++ linux/alias/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# echo alias cD="cd ~/Desktop" >> ~/.bashrc
-# echo alias GIT="cD; bash .gitupdate.sh" >> ~/.bashrc
+# echo alias cD=\"cd ~/Desktop\" >> ~/.bashrc
+# echo alias GIT=\"cD\; bash .gitupdate.sh\" >> ~/.bashrc
 # echo alias C_COMPILE; for i in *.c; do gcc $i -lm -o ${i%.*}.out ; done >> ~/.bashrc
-# echo alias RUN="bash run.sh" >> ~/.bashrc
+# echo alias RUN=\"bash run.sh\" >> ~/.bashrc
