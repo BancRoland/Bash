7d1a05b5926bc70509b071a5fda3e13e8ff1303c
roland <bancroland@gmail.com>
2023-09-01 20:48:11 +0200

    .

---
+++ scp/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FOLDER=/home/roland/Desktop/kuvik/ads-b/log/raw
+FILE=/home/roland/Desktop/kuvik/ads-b/log/raw
+IP="192.168.1.2"
+user=kuvik
+DEST=~/Desktop
+# scp -r $FOLDER $USER@$IP:$DEST
+scp $FILE $USER@$IP:$DEST
