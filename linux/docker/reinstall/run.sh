3ad466c6437d7e43ef0a14057679419f4111e2f9
BancRoland <bancroland@gmail.com>
2023-11-10 14:37:21 +0100

    .

---
+++ linux/docker/reinstall/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir .ssh
+cp ~/.ssh/* ./.ssh
+cat id_ed25519.pub
+
+docker build -t reinstall_image .
+docker run -it --name reinstall_docker reinstall_image
+
+# docker ps -a    # list containers
+# docker rm your-container-name   # remove container
+
+# docker exec -it cont_name /bin/bash
+# docker start 63a871f99bfc
+# docker stop 63a871f99bfc
